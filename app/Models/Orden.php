345d86c70296c2d88720f3f3079257e3fe3bbc1d
tsayjunior <juniorllanos15@gmail.com>
2023-12-19 20:16:51 -0400

    correcion al pagar

--- app/Models/Orden.php
+++ app/Models/Orden.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Orden extends Model
 {
     use HasFactory;
@@ -30,8 +30,14 @@ class Orden extends Model
     ];
 
     static function getNumberOrden(){
-        $ultima_orden = self::latest()->first()->id;
-        return $ultima_orden + 1;
+        // $ultima_orden = self::latest()->first()->id;
+        $nombreSecuencia = 'ordens_id_seq';
+
+        // Ejecutar la consulta SQL para obtener el próximo valor de la secuencia
+        $resultado = DB::select("SELECT nextval('$nombreSecuencia') AS siguiente_valor");
+        // Obtener el siguiente valor de la secuencia desde el resultado
+        $siguienteValor = $resultado[0]->siguiente_valor;
+        return $siguienteValor + 1;
     }
 
 
